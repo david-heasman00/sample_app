44e3b4b1825ef50310f0cb19ef3861a36f597003
David Heasman <d.heasman@gmail.com>
2012-07-18 08:16:03 +0100

    Added contact us page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -30,19 +30,20 @@ describe "StaticPages" do
 		end
 	end
 	
-	describe "About page" do
+	describe "Contact Us" do
 		
-		it "should have the content 'About Us'" do
-			visit '/static_pages/about'
-			page.should have_selector('h1', :text => 'About Us')
+		it "should have the content 'Contact Us'" do
+			visit '/static_pages/contact'
+			page.should have_selector('h1', :text => 'Contact Us')
 		end
 		
-		it "should have the title 'About Us'" do
-			visit '/static_pages/about'
+		it "should have the title 'Contact Us'" do
+			visit '/static_pages/contact'
 			page.should have_selector('title',
-						:text => "Ruby on Rails Tutorial Sample App | About Us")
+						:text => "Ruby on Rails Tutorial Sample App | Contact Us")
 		end
 	end	
+	
 end
 
 
